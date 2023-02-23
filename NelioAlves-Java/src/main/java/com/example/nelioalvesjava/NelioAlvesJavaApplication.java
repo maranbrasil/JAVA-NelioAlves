7d9bc248f9d63d4cf62321793dae2b01ad4ad571
maranbrasil <maranmorimoto@gmail.com>
2023-02-23 08:59:36 -0300

    starting a new repository for Java studies

---
+++ NelioAlves-Java/src/main/java/com/example/nelioalvesjava/NelioAlvesJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nelioalvesjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelioAlvesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NelioAlvesJavaApplication.class, args);
+	}
+
+}
