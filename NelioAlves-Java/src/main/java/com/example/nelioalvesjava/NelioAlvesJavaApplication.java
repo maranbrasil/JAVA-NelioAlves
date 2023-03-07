29bb9bd8852a7f85007837ae6f22603682676ef4
maranbrasil <maranmorimoto@gmail.com>
2023-03-06 21:02:10 -0300

    class 96 for

--- NelioAlves-Java/src/main/java/com/example/nelioalvesjava/NelioAlvesJavaApplication.java
+++ NelioAlves-Java/src/main/java/com/example/nelioalvesjava/NelioAlvesJavaApplication.java
@@ -8,5 +8,18 @@ public class NelioAlvesJavaApplication {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		String[] vect = new String[] {"Maria", "Bob", "Alex"};
+
+		for (int i= 0; i < vect.length; i++) {
+			System.out.println(vect[i]);
+		}
+
+		System.out.println("------------------------");
+		// essa é uma forma de usar um for.
+		// abro o for, informo o tipo que será retornado na iteracao, obj (é cada uma das linhas do vetor) , e ":" o vetor que queremos percorrer
+		// forma de ler "for (String obj : vect)": para cada objeto obj contido no vetor vect, faça:
+		for (String obj : vect) {
+			System.out.println(obj);
+		}
 	}
 }
