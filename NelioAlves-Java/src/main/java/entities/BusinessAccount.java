26256db655ed37eb27d9aeed22b265a451efca17
maranbrasil <maranmorimoto@gmail.com>
2023-04-01 13:38:47 -0300

    class156-heritage classes

---
+++ NelioAlves-Java/src/main/java/entities/BusinessAccount.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class BusinessAccount extends Account{ // bussiness acccount tem os mesmos metdos e atributos da classe account
+  private double loanLimit;
+
+  public BusinessAccount() {
+  }
+
+  public BusinessAccount(Integer number, String holder, Double balance, double loanLimit) {
+    super(number, holder, balance); // o super chama dentro os atributos da classe mãe desse construtor. OU seja executa a lógica do construtor da classe mae(super classe)
+    this.loanLimit = loanLimit;
+  }
+
+  public double getLoanLimit() {
+    return loanLimit;
+  }
+
+  public void setLoanLimit(double loanLimit) {
+    this.loanLimit = loanLimit;
+  }
+
+  public void Loan(double amount) {
+    if(amount <= loanLimit) {
+      // deposit(amount); // só consigo pegar o metodo da super classe pq eu transformei essa classe que estou mexendo em herança
+      balance += amount - 10.0; // dessa forma o balance fica com erro pois na classe mae o atributo é private(só quem é da mesma classe acessa o atributo). PAra acessar basta tranforma=lo em protected na classe mae.
+    }
+  }
+}
