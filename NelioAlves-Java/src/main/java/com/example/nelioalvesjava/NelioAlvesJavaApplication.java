aac6e7d80c8e7c671701e55a67cfadf3f8981ff9
maranbrasil <maranmorimoto@gmail.com>
2023-04-16 17:39:47 -0300

    implements position class

--- NelioAlves-Java/src/main/java/com/example/nelioalvesjava/NelioAlvesJavaApplication.java
+++ NelioAlves-Java/src/main/java/com/example/nelioalvesjava/NelioAlvesJavaApplication.java
@@ -1,12 +1,15 @@
 package com.example.nelioalvesjava;
 
+import boardgame.Position;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class NelioAlvesJavaApplication {
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-
+//		Locale.setDefault(Locale.US);
+//		Scanner sc = new Scanner(System.in);
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 }
