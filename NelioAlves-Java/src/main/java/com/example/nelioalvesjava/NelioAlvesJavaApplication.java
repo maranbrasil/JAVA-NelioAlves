86ad08d5dfd308d83d7c6616bed508b5d94892b8
maranbrasil <maranmorimoto@gmail.com>
2023-04-23 17:10:35 -0300

    handling exceptions and clearing terminal

--- NelioAlves-Java/src/main/java/com/example/nelioalvesjava/NelioAlvesJavaApplication.java
+++ NelioAlves-Java/src/main/java/com/example/nelioalvesjava/NelioAlvesJavaApplication.java
@@ -1,8 +1,10 @@
 package com.example.nelioalvesjava;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class NelioAlvesJavaApplication {
@@ -12,17 +14,26 @@ public class NelioAlvesJavaApplication {
 		ChessMatch chessMatch = new ChessMatch();
 
 		while(true) { //coloquei true, pq nao temos um metodo de checkmate pra encerrar a partida.
-			UI.printBoard(chessMatch.getPieces()); // imprimir tabuleiro na tela
-			System.out.println(); // salto um linha
-			System.out.print("Source: "); // usuario digitar a posicao de origem
-			ChessPosition source = UI.readChessPosition(sc);
+			try {
+				UI.clearScreen(); // lipa a tela do terminal para nao aparecer varios boards
+				UI.printBoard(chessMatch.getPieces()); // imprimir tabuleiro na tela
+				System.out.println(); // salto um linha
+				System.out.print("Source: "); // usuario digitar a posicao de origem
+				ChessPosition source = UI.readChessPosition(sc);
 
-			System.out.println();
-			System.out.print("Target: "); // usuario indicar a posicao de destino
-			ChessPosition target = UI.readChessPosition(sc);
+				System.out.println();
+				System.out.print("Target: "); // usuario indicar a posicao de destino
+				ChessPosition target = UI.readChessPosition(sc);
 
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target); // efetua o movimento da peça da orgiem para o destino
+				ChessPiece capturedPiece = chessMatch.performChessMove(source,
+						target); // efetua o movimento da peça da orgiem para o destino
+			} catch (ChessException e) {
+				System.out.println(e.getMessage()); // mostra o erro
+				sc.nextLine(); // aguarda o usuario digitar enter
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage()); // mostra o erro
+				sc.nextLine(); // aguarda o usuario digitar enter
+			}
 		}
-
 	}
 }
